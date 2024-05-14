d5b6ca6d460f860e5dcfa2f68f743a7007bf2dae
Abhinav Prakash <email@abhinav-prakash.de>
2024-05-14 21:54:31 +0530

    json deserialization

---
+++ src/main/java/org/kafka/consumer/config/KafkaConsumerConfig.java
@@ -0,0 +1,46 @@
+package org.kafka.consumer.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+
+    @Bean
+    public Map<String, Object> consumerConfig(){
+        Map<String, Object> properties= new HashMap<>();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "my-customer, my-cricket");
+        properties.put(JsonDeserializer.TRUSTED_PACKAGES,"org.kafka.producer.dto");
+
+        return properties;
+    }
+
+    @Bean
+    public ConsumerFactory<String,Object> consumerFactory(){
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Object>> kafkaListenerContainerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
--- src/main/java/org/kafka/consumer/service/MessageListener.java
+++ src/main/java/org/kafka/consumer/service/MessageListener.java
@@ -1,6 +1,7 @@
 package org.kafka.consumer.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.kafka.producer.dto.Customer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,17 +9,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class MessageListener {
 
-    @KafkaListener(topics = "NDTV", groupId = "news")
-    public void consumeMessagesA(String message){
+    @KafkaListener(topics = "customer", groupId = "my-customer", containerFactory = "kafkaListenerContainerFactory")
+    public void consumeMessagesA(Customer message){
         log.info("Consumer A consumed the message: [ {} ]", message);
     }
 
-    @KafkaListener(topics = "NDTV", groupId = "news")
-    public void consumeMessagesB(String message){
+    @KafkaListener(topics = "customer", groupId = "my-customer",containerFactory = "kafkaListenerContainerFactory")
+    public void consumeMessagesB(Customer message){
         log.info("Consumer B consumed the message: [ {} ]", message);
     }
 
-    @KafkaListener(topics = "NDTV", groupId = "news")
+    @KafkaListener(topics = "Cricket", groupId = "my-cricket",containerFactory = "kafkaListenerContainerFactory")
     public void consumeMessagesC(String message){
         log.info("Consumer C consumed the message: [ {} ]", message);
     }
